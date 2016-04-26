d688e56dc3ec4253938f5f375f229aa9e45503be
Richard Scothern <richard.scothern@gmail.com>
2016-04-26 16:59:43 -0700

    Detect yaml or subcommand arguments
    Signed-off-by: Richard Scothern <richard.scothern@docker.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,17 +2,9 @@
 
 set -e
 
-if [ -f $1 ]; then
-    set -- /bin/registry serve "$@"
-    exec "$@"
-fi
-
-if [ "$1" = "sh" ]; then
-    shift
-    set -- /bin/sh "$@"
-    exec "$@"
-fi
-
-set -- /bin/registry "$@"
+case "$1" in
+    *.yaml|*.yml) set -- registry serve "$@" ;;
+    serve|garbage-collect|help|-*) set -- registry "$@" ;;
+esac
 
 exec "$@"
